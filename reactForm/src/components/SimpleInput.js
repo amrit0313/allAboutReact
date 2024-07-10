8cae8b9639bccada2235fbaaf2bd090dfe9fa5ec
amrit0313 <amrtz0313@gmail.com>
2024-07-10 22:25:30 +0545

    updated and made code clean

--- reactForm/src/components/SimpleInput.js
+++ reactForm/src/components/SimpleInput.js
@@ -1,43 +1,33 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 //if we are only interested in checking once, ref might be better... beacause logging and upadating value on every keystroke isnt needed here
 //and if we want to validate it on every keystroke it would be nice using state, if we want to reset after submission state would come in use
 
 const SimpleInput = (props) => {
-  const inputChangeRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const inputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-    if (event.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    }
   };
 
   const inputBlurHandler = (event) => {
     setEnteredNameIsTouched(true);
-    if (event.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    } else {
-      setEnteredNameIsValid(false);
-    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameIsTouched(true);
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
     console.log(enteredName);
-    const enteredValue = inputChangeRef.current.value;
-    console.log(enteredValue);
-    setEnteredNameIsValid(true);
     setEnteredName("");
+    setEnteredNameIsTouched(false);
   };
-  const inputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
 
   const inputClasses = inputIsInvalid ? "form-control invalid" : "form-control";
   return (
@@ -45,7 +35,6 @@ const SimpleInput = (props) => {
       <div className={inputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={inputChangeRef}
           type="text"
           id="name"
           onBlur={inputBlurHandler}
