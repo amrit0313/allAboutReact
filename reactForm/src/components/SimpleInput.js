0d943f0b9430608620c04a49ccdcde3b72fc6bf8
amrit0313 <amrtz0313@gmail.com>
2024-07-11 10:06:28 +0545

    simpleInputForm Improved

--- reactForm/src/components/SimpleInput.js
+++ reactForm/src/components/SimpleInput.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //if we are only interested in checking once, ref might be better... beacause logging and upadating value on every keystroke isnt needed here
 //and if we want to validate it on every keystroke it would be nice using state, if we want to reset after submission state would come in use
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+  const [fromIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const inputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -44,7 +53,7 @@ const SimpleInput = (props) => {
         {inputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!fromIsValid}>Submit</button>
       </div>
     </form>
   );
